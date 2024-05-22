8a77a8df5a03169b1cce2a0cdb75921b15896fdd
zhangzhaochen <953320403@qq.com>
2024-05-22 10:22:45 +0800

    配置及mapper

---
+++ logistics-manager/logistics-manager-dao/src/main/java/com/zzc/mapper/BasicDataMapper.java
@@ -0,0 +1,31 @@
+package com.zzc.mapper;
+
+import com.zzc.pojo.BasicData;
+import com.zzc.pojo.BasicDataExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BasicDataMapper {
+    int countByExample(BasicDataExample example);
+
+    int deleteByExample(BasicDataExample example);
+
+    int deleteByPrimaryKey(Integer baseId);
+
+    int insert(BasicData record);
+
+    int insertSelective(BasicData record);
+
+    List<BasicData> selectByExample(BasicDataExample example);
+
+    BasicData selectByPrimaryKey(Integer baseId);
+
+    int updateByExampleSelective(@Param("record") BasicData record, @Param("example") BasicDataExample example);
+
+    int updateByExample(@Param("record") BasicData record, @Param("example") BasicDataExample example);
+
+    int updateByPrimaryKeySelective(BasicData record);
+
+    int updateByPrimaryKey(BasicData record);
+}
